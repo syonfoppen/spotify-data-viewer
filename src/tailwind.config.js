38d712e04fd65e15a9724791ec462718de7c9be4
Syon Foppen <ikbensyon@gmail.com>
2022-04-07 15:28:22 +0200

    first setup of the homescreen

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     content: ["./**/*.{razor,html,cshtml}"],
   theme: {
-    extend: {},
+    colors: {
+      'spotifygreen': {
+        800: '#1DB954',
+        900: '#138039',
+      },
+      'spotifyblack': '#191414'
+    }
   },
   plugins: [],
 }
